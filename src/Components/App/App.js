1eb70776a0cc9e4e4be7d9ff83d95c0b64238061
Sam Baldwin <sambaldwin92@gmail.com>
2023-04-25 08:30:31 +0100

    Initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+
+function App() {
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h2" component="h1" gutterBottom>Test</Typography>
+    </Container>
+  );
+}
+
+export default App;
